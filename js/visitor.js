1e725a6c557224824141247befbb3c3cbc83bb3f
Jim Porter <jporter@jnlcom.com>
2014-05-31 14:16:34 -0500

    marker title fixed and infowindow closer implemented

--- js/visitor.js
+++ js/visitor.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-// Construct the catalog query string
+
 url = 'http://data.nashville.gov/resource/bta3-7qkc.json';
 
 var center = new google.maps.LatLng(36.1866405,-86.7852455);
@@ -10,14 +10,31 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 $.getJSON(url, function(data, textstatus) {
+    openwindows = [];
     //console.log(data);
     $.each(data, function(i, entry) {
         console.log(entry);
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(entry.latitude, entry.longitude),
             map: map,
-            title: location.title
+            title: entry.title
+        });
+        contentString = '<h4>'+entry.title+'</h4><p>'+entry.description+'</p>';
+        infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 150
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+            openwindows.push(infowindow);
         });
     }); // end $.each
 }); // end $.getJSON
+google.maps.event.addListener(map,'click',function(){
+    console.log(openwindows);
+    for(var num = 0; num < openwindows.length;num++){
+        openwindows[num].close();
+    }
+    openwindows = [];
+});
 });
