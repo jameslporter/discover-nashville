4782085cc013bc9665ed9983e3430634fc33cc00
Jim Porter <jporter@jnlcom.com>
2014-05-31 19:55:56 -0500

    lolwut

--- js/fetchAndMark.js
+++ js/fetchAndMark.js
@@ -1,18 +1,18 @@
 function fetchAndMark(url,infoTemplate){
 
 $.getJSON(url, function(data, textstatus) {
-    openwindows = [];
+
     //console.log(data);
     $.each(data, function(i,entry){
-        console.log(entry);
         if(!entry.description){ entry.description = 'No description available.';}
+        if(!entry.park_notes){ entry.park_notes = 'No park notes provided.'; }
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(entry.latitude, entry.longitude),
             map: map,
             title: entry.title
         });
-        contentString = _.template(infoTemplate,entry);
-        infowindow = new google.maps.InfoWindow({
+        var contentString = _.template(infoTemplate,entry);
+        var infowindow = new google.maps.InfoWindow({
             content: contentString,
             maxWidth: 150
         });
--- js/visitor.js
+++ js/visitor.js
@@ -8,13 +8,19 @@ var mapOptions = {
     center: center
 }
 map = new google.maps.Map(document.getElementById("map"), mapOptions);
+openwindows = [];
+
 publicArtTemplate = '<h4><%= title %></h4><p><%= description %></p>';
 fetchAndMark(url,publicArtTemplate);
-google.maps.event.addListener(map,'click',function(){
-    console.log(openwindows);
-    for(var num = 0; num < openwindows.length;num++){
-        openwindows[num].close();
-    }
-    openwindows = [];
-});
+parkTemplate = '<h4><%= park_name %></h4><p><%= park_notes %></p>';
+//fetchAndMark('http://data.nashville.gov/resource/geur-py3i.json',parkTemplate);
+specialeventspart = 'https://data.nashville.gov/api/views/vygj-v677/rows.json?accessType=DOWNLOAD';
+fetchAndMark(specialeventspart,'special event');
+    google.maps.event.addListener(map,'click',function(){
+        console.log(openwindows);
+        for(var num = 0; num < openwindows.length;num++){
+            openwindows[num].close();
+        }
+        openwindows = [];
+    });
 });
