0e799e0d790e11573fad6c29bbbc27847815e99e
Jim Porter <jporter@jnlcom.com>
2014-06-01 14:28:23 -0500

    got the correct json endpoints now and a fix for data sources with mapped_location without lat/long as direct properties

--- js/fetchAndMark.js
+++ js/fetchAndMark.js
@@ -6,6 +6,7 @@ $.getJSON(url, function(data, textstatus) {
     $.each(data, function(i,entry){
         if(!entry.description){ entry.description = 'No description available.';}
         if(!entry.park_notes){ entry.park_notes = 'No park notes provided.'; }
+        if(entry.park_notes != null){ entry.longitude=entry.mapped_location.longitude;entry.latitude=entry.mapped_location.latitude;}
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(entry.latitude, entry.longitude),
             map: map,
--- js/visitor.js
+++ js/visitor.js
@@ -10,12 +10,18 @@ var mapOptions = {
 map = new google.maps.Map(document.getElementById("map"), mapOptions);
 openwindows = [];
 
-publicArtTemplate = '<h4><%= title %></h4><p><%= description %></p>';
+publicArtTemplate = '<h3>Public Art</h3><h4><%= title %></h4><p><%= description %></p>';
 fetchAndMark(url,publicArtTemplate);
-parkTemplate = '<h4><%= park_name %></h4><p><%= park_notes %></p>';
-//fetchAndMark('http://data.nashville.gov/resource/geur-py3i.json',parkTemplate);
+
+metro_public_art = 'http://data.nashville.gov/resource/eviu-nxp6.json';
+fetchAndMark(metro_public_art,'<h2><%= artwork %></h2><h3><%= first_name %> <%= last_name %></h3><h4><%= location %></h4> <p><%= description %></p><p><a href="<%= page_link %>">More Information</a></p>')
+
+
+parkTemplate = '<h3>Public Park</h3><h4><%= park_name %></h4><p><%= park_notes %></p>';
+fetchAndMark('http://data.nashville.gov/resource/geur-py3i.json',parkTemplate);
+
 specialeventspart = 'https://data.nashville.gov/api/views/vygj-v677/rows.json?accessType=DOWNLOAD';
-fetchAndMark(specialeventspart,'special event');
+//fetchAndMark(specialeventspart,'special event');
     google.maps.event.addListener(map,'click',function(){
         console.log(openwindows);
         for(var num = 0; num < openwindows.length;num++){
